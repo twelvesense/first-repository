bb9292c426a2e21736766b4fb9464c43edeeaef1
twelvesense <102462140+twelvesense@users.noreply.github.com>
2022-07-22 16:21:13 +0900

    Update test.py

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -1,1 +1,11 @@
-print('hello world!')
+import time
+import numpy as np
+import pandas as pd
+import matplotlib
+from matplotlib.backends.backend_agg import RendererAgg
+import streamlit as st
+from io import StringIO
+
+st.title('This is a title')
+st.header('This is a header')
+st.write('Hello, *World!* :sunglasses:')
